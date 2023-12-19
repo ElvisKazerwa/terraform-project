32246cec4494374e01ee478587ab4bced6aaf90e
elviskazerwa <110235165+ElvisKazerwa@users.noreply.github.com>
2023-12-19 14:38:12 +0100

    Update main.tf

--- Conditional_Expressions/main.tf
+++ Conditional_Expressions/main.tf
@@ -7,12 +7,14 @@ terraform {
   }
 }
 
+
 # Configuring the AWS Provider
 provider "aws"{
   region     = "eu-central-1"
   access_key = "AKIA6QEFRZJACXBRGGON"
   secret_key = "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"
 }
+
 
 # Creating the test-server instance if `istate` equals true
 resource "aws_instance" "test-server" {
